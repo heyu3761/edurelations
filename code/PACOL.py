e61bc8418b550d8e8110059e683a9e099a4420a7
heyu3761 <38234101+heyu3761@users.noreply.github.com>
2023-06-28 23:34:36 +0800

    Update PACOL.py

--- code/PACOL.py
+++ code/PACOL.py
@@ -14,15 +14,6 @@ from sklearn.metrics import classification_report
 from sklearn.model_selection import  train_test_split
 
 
-
-
-def cos(array1, array2):
-    norm1 = np.sqrt(sum(list(map(lambda x: np.power(x, 2), array1))))
-    norm2 = np.sqrt(sum(list(map(lambda x: np.power(x, 2), array2))))
-    return sum([array1[i]*array2[i] for i in range(0, len(array1))]) / (norm1 * norm2)
-
-
-
 def selectInstance(label_ind, unlabel_ind, model):
 
     ret1 = unlabel_ind.index[0]
@@ -35,29 +26,19 @@ def selectInstance(label_ind, unlabel_ind, model):
     ui = unlabel_ind.index
 
     for u in unlabel_ind.index:
-        p = int(dic[u])
-        y1 = y_p[u]
-        y2 = y_p[p]
-        p1 = pro[u]
-        p2 = pro[p]
-        p1 = max(p1)
-        p2 = max(p2)
-
-        if y1*p1+y2*p2!=0:
-            sim = 0
-            for sinu in ui:
-                sim1 = cos(X[u],X[sinu])
-                sim2 = cos(X[p],X[sinu])
-                sim = sim + sim1 + sim2
-            ans = abs(y1*p1+y2*p2)*sim
-            if ans > maxG:
-                maxG = ans
-                ret1 = u
-                ret2 = p
+        v = int(dic[u])
+        puv=pro[u][1]
+        pvu = pro[v][1]
+        # calculate evaluation function value
+        ans =1- abs(puv-pvu)
+        if ans > maxG:
+            maxG = ans
+            ret1 = u
+            ret2 = v
     return ret1,ret2
 
 
-excelFile = os.path.join('./','math.xlsx')
+excelFile = os.path.join('./','ESM.xlsx')
 df=pd.read_excel(excelFile)
 dataset = df.values
 
@@ -102,45 +83,29 @@ for round in range(1):
     saver.set_initial_point(test_auc)
 
     while len(unlab_ind.index)>0:
+        # select instance
         select_ind1, select_ind2 = selectInstance(label_ind, unlab_ind, model=model)
 
-        if select_ind1 in unlab_ind.index:
-            label_ind.update(select_ind1)
-            unlab_ind.difference_update(select_ind1)
-        else:
-            select_ind =  selectInstance(label_ind, unlab_ind, model=model, batch_size=1)
-            label_ind.update(select_ind)
-            unlab_ind.difference_update(select_ind)
-        if select_ind2 in unlab_ind.index:
-            #print(select_ind2 in unlab_ind.index)
-            label_ind.update(select_ind2)
-            unlab_ind.difference_update(select_ind2)
-            #print(select_ind2 in unlab_ind.index)
-        else:
-            select_ind =  selectInstance.select(label_ind, unlab_ind, model=model, batch_size=1)
-            label_ind.update(select_ind)
-            unlab_ind.difference_update(select_ind)
-        # Update model and calc performance according to the model you are using
+        # update data set
+        label_ind.update(select_ind1)
+        unlab_ind.difference_update(select_ind1)
+        label_ind.update(select_ind2)
+        unlab_ind.difference_update(select_ind2)
+
+
         model.fit(X=X[label_ind.index, :], y=y[label_ind.index])
         pred = model.predict(X[test_idx, :])
         y_predict_prob = model.predict_proba(X[test_idx, :])
         test_auc = metrics.roc_auc_score(y[test_idx],y_predict_prob,multi_class='ovr')
         auc_ret.append(copy.deepcopy(test_auc))
-        # Save intermediate results to file
+
         st = alibox.State(select_index=select_ind1, performance=test_auc)
         saver.add_state(st)
         st = alibox.State(select_index=select_ind2, performance=test_auc)
         saver.add_state(st)
-       # saver.save()
 
-        # Passing the current progress to stopping criterion object
-     #   stopping_criterion.update_information(saver)
-    # Reset the progress in stopping criterion object
-   # stopping_criterion.reset()
     unc_result.append(copy.deepcopy(saver))
 df=DataFrame(auc_ret)
-df.to_excel('./my/math_all_simall_ans.xlsx')
-print('./my/math_all_simall_ans.xlsx')
+df.to_excel('./ans.xlsx')
 analyser = alibox.get_experiment_analyser(x_axis='num_of_queries')
-print(analyser)
 analyser.plot_learning_curves(title='Example of AL', std_area=True)
